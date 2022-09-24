8edaf5fc96a2a146933822c0afbe689714a27d05
mmarishkaa <mariamiremadze.99@gmai.com>
2022-09-24 05:15:23 -0700

    my_commit

---
+++ day2/lesson2.1.py
@@ -0,0 +1,32 @@
+name = "mariami"
+print(len(name))
+my_sentence = """გამარჯობა შეგახსენებთ,რომ ს
+სწავლას ვიწყებთ 19სექტემბერს,
+გთხოვთ არ დააგვიანოთ"""
+print(my_sentence)
+if 10>3 :
+ print("nice")
+if 10<20 :
+    print("goodbye")
+
+
+full_name = "mariam iremadze"
+print(len(full_name))
+print(full_name[0])
+print(full_name[4])
+print("z" in full_name)
+print ("g" in full_name)
+print ("m" not in full_name)
+print(full_name[2:10])
+print(full_name[2:12:2])
+print(full_name[:5])
+print(full_name[3:])
+print(full_name [-1])
+print(full_name[-1:-12:-2])
+print(full_name[0]+"ariami")
+print(full_name.upper())
+name2 = "MARIAMI"
+print(name2.lower())
+name3 = '   mariami   '
+print(name3.replace(" ","^^"))
+print(name2.replace("M","#"))
