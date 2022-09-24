3b893b391a29d7e03cf074737d466f96494b22ac
mmarishkaa <mariamiremadze.99@gmai.com>
2022-09-24 06:13:09 -0700

    day.1 commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,12 @@
+name = "mariami"
+surname = "iremadze"
+age = 23
+heght = 156.5
+Knows_programing = False
+
+print (type(name))
+print (type(surname))
+print (type(age))
+print (type(heght))
+print (type(Knows_programing))
+print(name+ " "+surname+" "+str(age)+" "+str(heght)+" "+str(Knows_programing))
